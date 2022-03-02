01fa18378fe006518d0cbd667b0917f6a888d0bf
HuynhHuuu <HUU.H9155@sinhvienhoasen.edu.vn>
2022-03-02 17:03:26 +0700

    HuuD21

---
+++ D21/HuuD21/index.js
@@ -0,0 +1,49 @@
+console.log("Hello");
+
+
+const express = require("express");
+const server = express();
+const bodyParser = require("body-parser");
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+
+let arr=[
+{ name: 'Hữu', age: '22' },
+{ name: 'Trọng', age: '20' },
+{ name: 'Mạnh', age: '27' },
+{ name: 'Kiên', age: '22' }
+];
+server.get("/",(request,response)=>{
+    response.send("Hello các bạn")
+})
+
+server.post("/post",(request,respone)=>{
+    let temp=request.body;
+    console.log(temp);
+    arr.push(temp);
+    respone.send();
+
+})
+console.log(arr);
+server.put("/put",(request,respone)=>{
+    let temp=request.body;
+    let name = temp.name;
+    for (let i=0;i<arr.length;i++)
+    {
+        if (name==arr[i].name)
+        {
+            arr[i]={...temp};
+        }
+    }
+    respone.send({respone:arr});
+    
+})
+
+server.delete("/delete",(request,respone)=>{
+    
+})
+
+server.listen(3000,()=>{
+    console.log("Server is running");
+})
